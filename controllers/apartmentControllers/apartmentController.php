6c096c0a1b84d6200846717c219ce1d8f7da2330
Vandal-William <vandal.william@hotmail.com>
2023-06-09 14:18:18 +0200

    apartment

--- controllers/apartmentControllers/apartmentController.php
+++ controllers/apartmentControllers/apartmentController.php
@@ -8,6 +8,25 @@ require_once './database/client.php';
 class Apartment {
     
     function addApartment(){
+        
+        $db = new Database();
+
+        $connexion = $db->getconnection();
+
+        $title = $_POST["title"];
+        $description = $_POST["description"];
+        $mainPicture = $_POST["mainPicture"];
+        $picture360 = $_POST["picture360"];
+        $adress = $_POST["adress"];
+        $zipCode = $_POST["zipCode"];
+        $city = $_POST["city"];
+        $price = $_POST["price"];
+        $size = $_POST["size"];
+        $bedroom = $_POST["bedroom"];
+        $capacity = $_POST["capacity"];
+
+
+
 
     }
 
@@ -15,8 +34,27 @@ class Apartment {
         
     }
 
-    function getOneApartment(){
+    function getOneApartment($apartment_id){
+        // get avec parametre
+        $db = new Database();
+
+        $connexion = $db->getconnection();
+
+        // récupérer les infos d'un appartement 
+        $request = $conexion->prepare("
+            SELECT *
+            FROM apartment
+            WHERE apartment_id = :apartment_id
+        ");
         
+        $request->execute([':apartment_id' => $apartment_id]);
+
+        $apartmentInfos = $request->fetch(PDO::FETCH_ASSOC);
+
+        $connexion= null;
+
+        header('Content-Type: application/json');
+        echo json_encode($apartmentInfos);
     }
 
     function getAllApartment(){
@@ -29,14 +67,22 @@ class Apartment {
 
         // 3. je prépare ma requète
         $request = $connexion->prepare("
-            SELECT * 
-            FROM apartment
+        SELECT *
+        FROM apartment
+        LEFT JOIN apartment_rental ON apartment_rental_apartement_id = apartment_id
+        WHERE (
+            CURDATE() < apartment_rental.apartment_rental_start
+            OR CURDATE() > apartment_rental.apartment_rental_end
+            OR apartment_rental.apartment_rental_start IS NULL
+            OR apartment_rental.apartment_rental_end IS NULL
+        );
+        
         ");
         // 4. J'exécute ma requête
         $request->execute();
 
         // 5. je renvoie les données au front en json
-        $apartmentInfos = $request->fetch(PDO::FETCH_ASSOC);
+        $apartmentInfos = $request->fetchAll(PDO::FETCH_ASSOC);
 
         $connexion= null;
         
@@ -47,8 +93,25 @@ class Apartment {
 
     }
 
-    function deleteApartment(){
+    function deleteApartment($apartment_id){
+
+        $db = new Database();
+
+        $connexion = $db->getconnection();
         
+        $request = $connexion->prepare("
+            DELETE FROM apartment
+            WHERE apartment_id = :apartment_id
+        ");
+
+        $request->execute([':apartment_id' => $apartment_id]);
+
+        $connexion= null;
+
+        $message = "l'appartement à bien été suprimer";
+        header('Location: http://localhost:3000/messagePages/viewAllMessages.php=' . urlencode($message));
+        exit;
+
     }
 
 }
