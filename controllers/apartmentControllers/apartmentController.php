685825fb9e89cd55982556b1ee40a0df716dfb44
Vandal-William <vandal.william@hotmail.com>
2023-06-13 09:49:05 +0200

    add access cors in apartment controller

--- controllers/apartmentControllers/apartmentController.php
+++ controllers/apartmentControllers/apartmentController.php
@@ -263,6 +263,12 @@ class Apartment {
 
         $connexion= null;
 
+
+        header("Access-Control-Allow-Origin: http://localhost:3000");
+        header("Access-Control-Allow-Methods: GET, POST");
+        header("Access-Control-Allow-Headers: Content-Type");
+        header("Access-Control-Allow-Credentials: true");
+
         header('Content-Type: application/json');
         echo json_encode($apartmentInfos);
     }
